61971f691c3cabfec69a449af9df8dd8581ec310
Mily <mp_morones@hotmail.com>
2021-07-09 12:42:51 -0500

    Implement logic for calculate and operate methods

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,7 +1,50 @@
-// import operate from './operate';
+import operate from './operate';
 
 const calculate = ({ calcObj, btnName }) => {
-  // const { total, next, operation} = calcObj;
+  let { total, next, operation } = calcObj;
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const getTotal = total;
+  let getNext = next;
+  let getOperation = operation;
+  let resTotal;
+
+  if (btnName === '+/-') {
+    parseInt(getNext, 10);
+    getNext *= -1;
+    resTotal = operate(getTotal, getNext, btnName);
+  } else if (btnName === '.') {
+    if (getNext !== '0') {
+      getNext = `${getNext}.`;
+    } else {
+      getNext = '0.';
+    }
+  } else if (btnName === '=') {
+    resTotal = operate(getTotal, parseInt(getNext, 10), btnName);
+  } else if (numbers.includes(btnName)) {
+    getNext = btnName;
+    resTotal = operate(getTotal, parseInt(getNext, 10), btnName);
+  } else if (btnName === 'AC') {
+    resTotal = 0;
+  } else {
+    resTotal = operate(getTotal, parseInt(getNext, 10), btnName);
+    if (btnName === '-') {
+      getOperation = 'subtraction';
+    } else if (btnName === '+') {
+      getOperation = 'addition';
+    } else if (btnName === 'x') {
+      getOperation = 'multiplication';
+    } else if (btnName === '÷') {
+      getOperation = 'division';
+    } else if (btnName === '%') {
+      getOperation = 'module';
+    }
+  }
+
+  total = resTotal;
+  next = toString(resTotal);
+  operation = getOperation;
+
+  return calcObj;
 };
 
 export default calculate;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,7 +1,21 @@
-// import Big from 'big.js';
+import Big from 'big.js';
 
 const operate = ({ numberOne, numberTwo, operation }) => {
-  
+  let total;
+  const oneBig = Big(numberOne);
+  const twoBig = Big(numberTwo);
+  if (operation === '-') {
+    total = oneBig.minus(twoBig);
+  } else if (operation === '+') {
+    total = oneBig.plus(twoBig);
+  } else if (operation === 'x') {
+    total = oneBig.times(twoBig);
+  } else if (operation === '÷') {
+    total = oneBig.div(twoBig);
+  } else if (operation === '%') {
+    total = oneBig.mod(twoBig);
+  }
+  return total;
 };
 
 export default operate;
