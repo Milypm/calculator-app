de498f7e28d7ed3f62651aa91f7a4389a0727c84
Mily <mp_morones@hotmail.com>
2021-07-09 16:21:39 -0500

    Fix App to be a class component (add constructor, state, render), implement handleClick function and pass it to ButtonPanel and Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,20 @@
 import PropTypes from 'prop-types';
 import '../style/style.css';
 
-const Button = ({ btnName }) => {
+const Button = ({ props }) => {
+  const { btnName, clickHandler } = props;
   const color = btnName === '÷' || btnName === 'x' || btnName === '-' || btnName === '+' || btnName === '=' ? 'orange' : 'grey';
   const width = btnName === '0' ? 'large' : 'normal';
   return (
-    <button type="button" className={`button-container ${color} ${width}`}>
+    <button type="button" className={`button-container ${color} ${width}`} onClick={clickHandler}>
       <span>{btnName}</span>
     </button>
   );
 };
 
 Button.propTypes = {
+  props: PropTypes.func.isRequired,
+  clickHandler: PropTypes.func.isRequired,
   btnName: PropTypes.string.isRequired,
 };
 
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,39 +1,50 @@
 import React from 'react';
-import '../style/style.css';
+import PropTypes from 'prop-types';
 import Button from './Button';
+import '../style/style.css';
 
-const ButtonPanel = () => (
-  <div className="btnPanel-container">
-    <div className="btns-row">
-      <Button btnName="AC" />
-      <Button btnName="+/-" />
-      <Button btnName="%" />
-      <Button btnName="÷" />
+const ButtonPanel = ({ props }) => {
+  const { clickHandler } = props;
+  // const handleClick = (btnName) => props.clickHandler(btnName);
+
+  return (
+    <div className="btnPanel-container">
+      <div className="btns-row">
+        <Button btnName="AC" clickHandler={clickHandler('AC')} />
+        <Button btnName="+/-" clickHandler={clickHandler('+/-')} />
+        <Button btnName="%" clickHandler={clickHandler('%')} />
+        <Button btnName="÷" clickHandler={clickHandler('÷')} />
+      </div>
+      <div className="btns-row">
+        <Button btnName="7" clickHandler={clickHandler('7')} />
+        <Button btnName="8" clickHandler={clickHandler('8')} />
+        <Button btnName="9" clickHandler={clickHandler('9')} />
+        <Button btnName="x" clickHandler={clickHandler('x')} />
+      </div>
+      <div className="btns-row">
+        <Button btnName="4" clickHandler={clickHandler('4')} />
+        <Button btnName="5" clickHandler={clickHandler('5')} />
+        <Button btnName="6" clickHandler={clickHandler('6')} />
+        <Button btnName="-" clickHandler={clickHandler('-')} />
+      </div>
+      <div className="btns-row">
+        <Button btnName="1" clickHandler={clickHandler('1')} />
+        <Button btnName="2" clickHandler={clickHandler('2')} />
+        <Button btnName="3" clickHandler={clickHandler('3')} />
+        <Button btnName="+" clickHandler={clickHandler('+')} />
+      </div>
+      <div className="btns-row">
+        <Button btnName="0" clickHandler={clickHandler('0')} />
+        <Button btnName="." clickHandler={clickHandler('.')} />
+        <Button btnName="=" clickHandler={clickHandler('=')} />
+      </div>
     </div>
-    <div className="btns-row">
-      <Button btnName="7" />
-      <Button btnName="8" />
-      <Button btnName="9" />
-      <Button btnName="x" />
-    </div>
-    <div className="btns-row">
-      <Button btnName="4" />
-      <Button btnName="5" />
-      <Button btnName="6" />
-      <Button btnName="-" />
-    </div>
-    <div className="btns-row">
-      <Button btnName="1" />
-      <Button btnName="2" />
-      <Button btnName="3" />
-      <Button btnName="+" />
-    </div>
-    <div className="btns-row">
-      <Button btnName="0" />
-      <Button btnName="." />
-      <Button btnName="=" />
-    </div>
-  </div>
-);
+  );
+};
+
+ButtonPanel.propTypes = {
+  props: PropTypes.func.isRequired,
+  clickHandler: PropTypes.func.isRequired,
+};
 
 export default ButtonPanel;
