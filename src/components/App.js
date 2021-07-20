2b3dc8a767916f6f2c97f5810b8905351dbb6f66
Mily <mp_morones@hotmail.com>
2021-07-20 11:52:34 -0500

    Refactor using hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const App = () => {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
 
-  handleClick(btnName) {
-    const { total, next, operation } = this.state;
+  const handleClick = (btnName) => {
     const obj = { total, next, operation };
     const calcObj = calculate(obj, btnName);
-    this.setState({
-      total: calcObj.total,
-      next: calcObj.next,
-      operation: calcObj.operation,
-    });
-  }
+    setTotal(calcObj.total);
+    setNext(calcObj.next);
+    setOperation(calcObj.operation);
+  };
 
-  render() {
-    const { next } = this.state;
-    return (
-      <>
-        <Display value={next} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Display value={next} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
+};
 
 export default App;
